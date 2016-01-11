eda294200e667887c815661c5fde7b48686fab29
RienMoeyaert <rien@bitsoflove.be>
2016-01-11 11:08:49 +0100

    Added multitenancy

--- Entities/File.php
+++ Entities/File.php
@@ -3,6 +3,7 @@
 use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Media\ValueObjects\MediaPath;
+use Modules\Media\Traits\MultiSiteTenancyTrait;
 
 /**
  * Class File
@@ -12,6 +13,8 @@ use Modules\Media\ValueObjects\MediaPath;
 class File extends Model
 {
     use Translatable;
+    use MultiSiteTenancyTrait;
+
     /**
      * All the different images types where thumbnails should be created
      * @var array
